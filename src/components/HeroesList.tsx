9aff664a75327dcc2bd62d3328bf5fa29148db3c
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-06-23 19:59:46 -0400

    more router

--- src/components/HeroesList.tsx
+++ src/components/HeroesList.tsx
@@ -1,6 +1,8 @@
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Hero } from "../types/hero";
-import HeroDetail from "./HeroDetail";
+import { Link } from "react-router-dom";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export default function HeroesList() {
   // so that it doesn't fetch twice with the strict mode
@@ -8,12 +10,9 @@ export default function HeroesList() {
 
   const [heroes, setHeroes] = useState<Hero[]>([]);
 
-  const [selectedHeroId, setSelectedHeroId] = useState<number | null>(null);
-  const selectedHero = heroes.find((hero) => hero.id === selectedHeroId);
-
   useEffect(() => {
     if (!fetched.current) {
-      fetch("http://localhost:3000/heroes")
+      fetch(`${apiUrl}/heroes`)
         .then((res) => {
           return res.json();
         })
@@ -24,32 +23,15 @@ export default function HeroesList() {
     }
   }, []); // execute once
 
-  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const updatedName = event.target.value;
-
-    setHeroes((prevHeroes) =>
-      prevHeroes.map((hero) => {
-        if (hero.id === selectedHeroId) {
-          return { ...hero, name: updatedName };
-        }
-        return hero;
-      })
-    );
-  };
-
-  const handleSelectHeroId = (id: number) => {
-    setSelectedHeroId(id);
-  };
-
   return (
     <>
       <h2 className="text-2xl">My heroes</h2>
       <ul className="flex flex-col gap-2 my-3">
         {heroes.map((hero) => (
-          <li
+          <Link
+            to={`/heroes/${hero.id}`}
             key={hero.id}
             className="flex cursor-pointer"
-            onClick={() => handleSelectHeroId(hero.id)}
           >
             <span className="bg-slate-700 text-white rounded-l p-2">
               {hero.id}
@@ -57,11 +39,9 @@ export default function HeroesList() {
             <span className="p-2 bg-slate-300 rounded-r w-full">
               {hero.name}
             </span>
-          </li>
+          </Link>
         ))}
       </ul>
-
-      <HeroDetail hero={selectedHero} onNameChange={handleNameChange} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import Dashboard from "./components/Dashboard.tsx";
 import HeroesList from "./components/HeroesList.tsx";
+import HeroDetail from "./components/HeroDetail.tsx";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
       { index: true, element: <Navigate replace to="/dashboard" /> },
       { path: "/dashboard", element: <Dashboard /> },
       { path: "/heroes", element: <HeroesList /> },
+      { path: "/heroes/:id", element: <HeroDetail /> },
     ],
   },
 ]);
