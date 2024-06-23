55a5704a9bc5d0554b928ffef399bf0da303eb66
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-06-23 18:25:13 -0400

    reinstall tailwind and practice use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,35 @@
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
+import "./App.css";
+
+type Hero = { id: number; name: string };
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [hero, setHero] = useState<Hero>({ id: 1, name: "Spiderman" });
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setHero({ ...hero, name: event.target.value });
+  };
+
+  return (
+    <div className="container mt-5 mx-auto">
+      <h2 className="text-2xl">Details</h2>
+      <div>
+        <span className="font-bold">ID: </span> {hero.id}
+      </div>
+      <div className="space-x-2">
+        <span className="font-bold">Name: </span>
+        <span className="uppercase">{hero.name}</span>
+      </div>
+      <div className="flex flex-col gap-2 mt-3 border-t">
+        <label>Hero name</label>
+        <input
+          type="text"
+          placeholder="name"
+          className="border border-gray-300 rounded-lg p-2 w-1/4"
+          value={hero.name}
+          onChange={handleNameChange}
+        />
+      </div>
+    </div>
+  );
 }
